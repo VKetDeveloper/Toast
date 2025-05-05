3dee4eb852c87fee17789c5f5da49b8deb2fcacc
Creat <ymandai@creator-net.com>
2025-05-05 10:58:13 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VketToast",
-            targets: ["Toast"]),
+            targets: ["VketToast"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
