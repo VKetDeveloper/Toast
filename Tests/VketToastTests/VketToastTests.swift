86f90babbb14830095f44e96de0db697ffd59145
Creat <ymandai@creator-net.com>
2025-05-05 11:28:18 +0900

    fix

--- Tests/VketToastTests/VketToastTests.swift
+++ Tests/VketToastTests/VketToastTests.swift
@@ -1,5 +1,5 @@
 import Testing
-@testable import Toast
+@testable import VketToast
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
